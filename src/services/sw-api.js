ee0c5f4e8e5986776afe0768f4b1d55716c24361
Albert Chung <chung972@gmail.com>
2019-07-08 07:49:07 -0500

    creates functions for fetching data from all starship pages from swapi

---
+++ src/services/sw-api.js
@@ -0,0 +1,28 @@
+export function allShipsPageOne(){
+    const endpoint = 'https://swapi.co/api/starships/?page=1';
+    return fetch(endpoint, {mode: "cors"}).then(res => res.json());
+}
+
+export function allShipsPageTwo(){
+    const endpoint = 'https://swapi.co/api/starships/?page=2';
+    return fetch(endpoint, {mode: "cors"}).then(res => res.json());
+}
+
+export function allShipsPageThree(){
+    const endpoint = 'https://swapi.co/api/starships/?page=3';
+    return fetch(endpoint, {mode: "cors"}).then(res => res.json());
+}
+
+export function allShipsPageFour(){
+    const endpoint = 'https://swapi.co/api/starships/?page=4';
+    return fetch(endpoint, {mode: "cors"}).then(res => res.json());
+}
+
+
+
+// const URL_BASE  = `https://swapi.co/api/starships/?page=$`
+
+// function getShips (base, num, mode) {
+//     let url = `${base}${num}`
+//     return fetch(url, mode).then(res => res.json)
+// }
