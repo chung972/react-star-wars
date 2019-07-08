4fb39c8bcb82a21bbd810425568953876047d59b
Albert Chung <chung972@gmail.com>
2019-07-08 09:16:27 -0500

    imports from react-router-dom; initializes starshipList to an empty array; creates getStarship(); adds switches, routes, and links

--- src/App.js
+++ src/App.js
@@ -1,13 +1,14 @@
-import React, {Component} from 'react';
+import React, { Component } from 'react';
 import './App.css';
+import { Route, Switch, Link } from 'react-router-dom';
 import { allShipsPageOne, allShipsPageTwo, allShipsPageThree, allShipsPageFour } from "./services/sw-api";
-import Starships from "./components/Starships/Starships";
+import StarshipPage from "./pages/StarshipPage/StarshipPage";
 
 
 class App extends Component {
 
   state = {
-    starshipList: null
+    starshipList: []
   }
 
   async componentDidMount() {
@@ -15,30 +16,58 @@ class App extends Component {
     const pageTwo = await allShipsPageTwo();
     const pageThree = await allShipsPageThree();
     const pageFour = await allShipsPageFour();
-    
+
     const shipList = [...pageOne.results, ...pageTwo.results, ...pageThree.results, ...pageFour.results]
     this.setState({
       starshipList: shipList
     });
   }
 
-  
+  getStarship = (index) => {
+    return this.state.starshipList[index];
+  }
+
+
+
   render() {
-    console.log("in render")
     console.log(this.state.starshipList);
     return (
       <div className="App">
         <header className="App-header">
           Star Wars API
         </header>
-        <Starships 
-          starshipList={this.state.starshipList} 
-        />
-      
+
+        <Switch>
+          <Route exact path='/'>
+            <div>
+              {this.state.starshipList && this.state.starshipList.map((starship, index) =>
+                <div>
+                  <Link
+                    to={`/starships/${index}`}
+                    key={index} 
+                  >
+                    {starship.name}
+                  </Link>
+                </div>
+              )}
+            </div>
+          </Route>
+
+
+
+          <Route path='/starships/:idx' render={(props) =>
+            <StarshipPage
+              {...props}
+              getStarship={this.getStarship}
+            />
+          } />
+
+        </Switch>
+
       </div>
     );
   }
-  
+
 }
 
 export default App;
